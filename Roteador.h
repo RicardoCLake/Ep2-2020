4d4697479e6bf6cae213c2b2f782a7897ab35dad
RicardoCLake <ricardodolago@gmail.com>
2020-11-30 12:51:49 -0300

    Commit normal

--- Roteador.h
+++ Roteador.h
@@ -1,18 +1,21 @@
 #ifndef ROTEADOR_H
 #define ROTEADOR_H
 
+#include "No.h"
 #include "TabelaDeRepasse.h"
 
-class Roteador
+class Roteador : public No
 {
     public:
         Roteador(int endereco);
         virtual ~Roteador();
         virtual TabelaDeRepasse* getTabela();
+        virtual void processar();
 
     protected:
 
     private:
+        TabelaDeRepasse* tab;
 };
 
 #endif // ROTEADOR_H
--- vaigit.sh
+++ vaigit.sh
@@ -1,5 +1,12 @@
 #! /bin/bash
 
+if [ $# -lt 1 ]; then
+   echo "Commit sera enviado com mensagem padrao"
+   git add .
+   git commit -m "Commit normal"
+   git push
+fi
+
 git add .
 git commit -m "$1"
 git push
