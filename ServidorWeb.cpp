bb6ac43b21778dacacae23a15d4fd9640b263c03
RicardoCLake <ricardodolago@gmail.com>
2020-11-30 23:08:35 -0300

    implementei ServidorWeb e outras arrumadinhas

--- ServidorWeb.cpp
+++ ServidorWeb.cpp
@@ -1,11 +1,20 @@
 #include "ServidorWeb.h"
 
-ServidorWeb::ServidorWeb(int endereco, int porta, Roteador* gateway, string conteudo)
+ServidorWeb::ServidorWeb(int endereco, int porta, Roteador* gateway, string conteudo):
+Processo(endereco, porta, gateway)
 {
-
+    this->conteudo = conteudo;
 }
 
 ServidorWeb::~ServidorWeb()
 {
 
 }
+
+void ServidorWeb::receber(int origem, Segmento* mensagem) 
+{
+    Segmento* seg = new Segmento(mensagem->getPortaDeDestino(), mensagem->getPortaDeOrigem(), this->conteudo);
+    Datagrama* dat = new Datagrama(this->endereco, origem, Processo::getTtlPadrao(), seg);
+    this->gateway->receber(dat);
+}
+
--- ServidorWeb.h
+++ ServidorWeb.h
@@ -2,15 +2,19 @@
 #define SERVIDORWEB_H
 
 #include "Roteador.h"
+#include "Processo.h"
+#include <string>
 
-class ServidorWeb
+class ServidorWeb : public Processo
 {
     public:
         ServidorWeb(int endereco, int porta, Roteador* gateway, string conteudo);
         virtual ~ServidorWeb();
+        virtual void receber(int origem, Segmento* mensagem);
     protected:
 
     private:
+        string conteudo;
 };
 
 #endif // SERVIDORWEB_H
